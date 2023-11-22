64e1fb34f3ec166ede3ed0b7833e9a5d9015fa7a
DenkyOgbonnaya <denkyogb@gmail.com>
2023-11-22 23:11:47 +0100

    fix:resolve social icons files

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,8 +3,8 @@ export { default as ChevronUp } from "./svgs/chevronUp.svg";
 export { default as ChevronDown } from "./svgs/chevronDown.svg";
 export { default as Rectangle } from "./svgs/rectangle.svg";
 export { default as Rectangle2 } from "./svgs/rectangle2.svg";
-export { default as Twitter } from "./svgs/twitter.svg";
-export { default as Telegram } from "./svgs/telegram.svg";
-export { default as Discord } from "./svgs/discord.svg";
+export { default as Twitter } from "./svgs/Twitter.svg";
+export { default as Telegram } from "./svgs/Telegram.svg";
+export { default as Discord } from "./svgs/Discord.svg";
 export {default as MenuIcon} from "./svgs/menu.svg"
 export {default as MenuCancel} from "./svgs/menuCancel.svg"
